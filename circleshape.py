c35332bd5fccbeee6860bf9994cda4d208f11cc0
zaquel92 <lucamit@pm.me>
2026-05-06 17:21:51 +0200

    add player.py circleshape.py, updated main.py constants.py, spaceship on screen

---
+++ circleshape.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import LINE_WIDTH
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # must override
+        self.screen = screen
+        pygame.draw.polygon(self.screen, "white", self.triangle(), LINE_WIDTH)
+
+    def update(self, dt):
+        # must override
+        pass
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Start of game loop
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        # to reduce screen refresh
         clock.tick(60)
         dt = clock.tick(60) / 1000
 
