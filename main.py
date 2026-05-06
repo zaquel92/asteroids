797d7cedde69bebf41ff54d64fe4ccc28dade375
zaquel92 <lucamit@pm.me>
2026-05-06 16:42:54 +0200

    update main.py reduced clock.tick

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    pygame.screen.fill("black")
-    pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids with pygame version: " + pygame.version.ver)
     print(
-        f"Screen width: {SCREEN_WIDTH}\
-        Screen height: {SCREEN_HEIGHT}"
-        )
+         f"Screen width: {SCREEN_WIDTH}\
+         Screen height: {SCREEN_HEIGHT}"
+         )
 
 
 if __name__ == "__main__":
