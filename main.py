5b76f78ca296642dc4fbc89c8ee6edf8325e6057
zaquel92 <lucamit@pm.me>
2026-05-06 15:25:57 +0200

    init project

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids with pygame version: " + pygame.version.ver)
+    print(
+	f"Screen width: {SCREEN_WIDTH}\
+	Screen height: {SCREEN_HEIGHT}"
+	)
+
+
+if __name__ == "__main__":
+    main()
